4ad1c0ab29b8d559fe34a3ba3957fea51553e5cd
therick96 <erick25gomez@gmail.com>
2017-12-07 13:40:56 -0400

    Paso de parametros por url

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__) #Objeto
 
@@ -6,4 +7,16 @@ app = Flask(__name__) #Objeto
 def index():
     return "Hola mundo" #Regresa string
 
-app.run(port=8001) #Ejecuta el servidor, puerto default: 5000
+@app.route('/p')
+def pagina_2():
+    param = request.args.get('param', 'no hay parametro') #Paso de parametros get
+    return param 
+
+@app.route('/p2')
+@app.route('/p2/<name>')
+@app.route('/p2/<int:num>')
+@app.route('/p2/<name>/<int:num>')
+def pagina3(name = '', num = ''):
+    return '{} {}'.format(name,num) #otra forma de pasar parametros get
+
+app.run(debug=True, port=8001) #Ejecuta el servidor, puerto default: 5000
