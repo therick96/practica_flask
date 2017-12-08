edb4c25c033782cef0d5cd26893100895ffaaa84
therick96 <erick25gomez@gmail.com>
2017-12-08 10:33:01 -0400

    Limpiada pagina

--- run.py
+++ run.py
@@ -7,39 +7,6 @@ from flask import render_template #Para renderizar template
 
 app = Flask(__name__) #Objeto
 
-@app.route('/') #wrap o decorador con la ruta
-@app.route('/index.html')
-def index():
-    web = "Web de Prueba"
-    #return "Hola mundo" #Regresa string
-    return render_template('index.html', web=web)
 
-@app.route('/about.html')
-def about():
-    return render_template('about.html', web="Nosotros")
-
-@app.route('/blog.html')
-def blog():
-    return render_template('blog.html', web="Blog")
-
-@app.route('/gallery.html')
-def gallery():
-    return render_template('gallery.html', web="Galeria")
-
-@app.route('/contact.html')
-def contact():
-    return render_template('contact.html', web="Contactenos")
-
-@app.route('/p')
-def pagina_2():
-    param = request.args.get('param', 'no hay parametro') #Paso de parametros get
-    return param 
-
-@app.route('/p2')
-@app.route('/p2/<name>')
-@app.route('/p2/<int:num>')
-@app.route('/p2/<name>/<int:num>')
-def pagina3(name = '', num = ''):
-    return '{} {}'.format(name,num) #otra forma de pasar parametros get
 
 app.run(debug=True, port=8001) #Ejecuta el servidor, puerto default: 5000
