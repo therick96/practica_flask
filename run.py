ba4caca85217ec43862c7069fe372a6c927dcebb
therick96 <erick25gomez@gmail.com>
2017-12-08 12:06:53 -0400

    Creada nueva pag

--- run.py
+++ run.py
@@ -7,6 +7,26 @@ from flask import render_template #Para renderizar template
 
 app = Flask(__name__) #Objeto
 
+@app.route("/")
+def index():
+    return render_template("index.html", titulo="Inicio", clase_header='class="alt"')
+
+@app.route("/About")
+def about():
+    return render_template("generic.html", titulo="About", clase_body='class="subpage"')
+
+@app.route("/extras")
+def extras():
+    return render_template("elements.html", titulo="Extras", clase_body='class="subpage"')
+
+@app.route("/login")
+def login():
+    return render_template("login.html", titulo="Log In", clase_body='class="subpage"')
+
+@app.route("/SignUp")
+def signup():
+    return render_template("register.html", titulo="Sign Up", clase_body='class="subpage"')
+
 
 
 app.run(debug=True, port=8001) #Ejecuta el servidor, puerto default: 5000
